ca9637597204d7d767db7324790bd01bbe8173b9
Mohamed Maache <92587524+momomuchu@users.noreply.github.com>
2023-05-16 15:41:45 +0000

    fe

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def process_image():
     image_key = 'uploads/' + image.filename
     s3.upload_fileobj(image, os.environ.get('S3_BUCKET_NAME'), image_key)
 
-    rekognition
+    
     resized_image = rekognition.detect_faces(
         Image={'S3Object': {'Bucket': os.environ.get('S3_BUCKET_NAME'), 'Name': image_key}},
         Attributes=['ALL']
